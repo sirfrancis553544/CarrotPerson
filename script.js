1d8e0cf5c2f3a4fc5196c765776489291965fcc3
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-16 22:50:03 +0800

    list of to dos

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-let theme = localStorage.getItem("theme")
+let theme = localStorage.getItem("theme");
 if (theme == null) {
-    setTheme("light")
+  setTheme("light");
 } else {
-    setTheme(theme)
+  setTheme(theme);
 }
-
+/* 
+TODO: submit Form validation needed
+TODO: Add Images of past works 
+TODO: Add gallery of images in about me section
+*/
 
 let themeDots = document.getElementsByClassName("theme-dot");
 
 for (var i = 0; themeDots.length > i; i++) {
   themeDots[i].addEventListener("click", function () {
     let mode = this.dataset.mode;
-   setTheme(mode)
+    setTheme(mode);
   });
 }
 
 function setTheme(mode) {
-    if (mode == "light") {
-        document.getElementById("theme-style").href = "default.css";
-    }
-    if (mode == "blue") {
-        document.getElementById("theme-style").href = "blue.css";
-    }
-    if (mode == "green") {
-        document.getElementById("theme-style").href = "green.css";
-    }
-    if (mode == "purple") {
-        document.getElementById("theme-style").href = "purple.css";
-    }
-    
-    localStorage.setItem("theme", mode)
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+
+  localStorage.setItem("theme", mode);
 }
 
 var year = new Date();
 
-document.getElementById("thisYear").innerHTML = `<div> © ${year.getFullYear()} </div>`;
+document.getElementById(
+  "thisYear"
+).innerHTML = `<div> © ${year.getFullYear()} </div>`;
