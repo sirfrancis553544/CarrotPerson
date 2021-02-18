ffd34c8136645c752966d4e74679455f6ae2417f
sirfrancis553544 <55309898+sirfrancis553544@users.noreply.github.com>
2021-02-18 16:11:32 +0800

    minimized images
    optimized images

--- script/script.js
+++ script/script.js
@@ -38,23 +38,22 @@ document.getElementById(
 ).innerHTML = `<div> <strong>©</strong> ${year.getFullYear()} Daniel Francis</div>`;
 
 today = new Date();
-var countDown = new Date(today.getFullYear(), 12, 2);
+
+var countDown = new Date(today.getFullYear(), 4, 28); // 4 - month & 28 day it ends
 if (today.getMonth() == 11 && today.getDate() > 25) {
   countDown.setFullYear(countDown.getFullYear() + 1);
 }
 var one_day = 1000 * 60 * 60 * 24;
 var days = 99;
 document.getElementById("countdown").innerHTML = `
-<h1>
-Find me on  </h1>
+<h1> Day <strong class="red">
+${Math.ceil(days - (countDown.getTime() - today.getTime()) / one_day)}
+</strong> of my 100 days of coding challenge   </h1>
+`;
 
-<a target="_blank" href="https://github.com/sirfrancis553544/oneHundredDaysOfCoding">
-<i class="fa fa-github" style="font-size: 48px"></i>
-</a></h1>`;
+// `<h1>
+// Find me on  </h1>
 
-
-{/* <h1> Day <strong class="red">
-
-${Math.ceil((days - (countDown.getTime() - today.getTime()) / one_day) )}
-
-</strong> of my 100 days of coding challenge   */}
+// <a target="_blank" href="https://github.com/sirfrancis553544/oneHundredDaysOfCoding">
+// <i class="fa fa-github" style="font-size: 48px"></i>
+// </a></h1>`
